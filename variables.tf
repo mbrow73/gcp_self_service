c5cf8c722592ba00baca0a5e3acceda66c3af501
mbrow73 <fatsaltytryhard@gmail.com>
2025-02-16 14:51:55 -0700

    script

--- variables.tf
+++ variables.tf
@@ -16,3 +16,13 @@ variable "projects" {
     }))
   }))
 }
+
+variable "project" {
+  type        = string
+  description = "The GCP project ID." 
+}
+
+variable "GOOGLE_CREDENTIALS" {
+  type        = string
+  description = "The GCP project ID." 
+}
