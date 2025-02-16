39399ddc8b62bb993b1a545a21f940b7ac8b5b5a
mbrow73 <fatsaltytryhard@gmail.com>
2025-02-15 17:51:55 -0700

    fixing rules

--- projects/my-service-project/egress.tf
+++ projects/my-service-project/egress.tf
@@ -2,14 +2,13 @@
 module "db_access" {
   source = "modules/egress_allow"
 
-  project_id    = "team-project-123"
-  network_name  = "secure-vpc"
+  project_id    = "eminent-torch-451100-r2"
+  network_name  = "default"
 
   egress_rules = [{
     rule_name   = "egress-postgres",    # Direction in name
     to_ranges   = ["10.100.20.0/24"],   # TO these IPs
     via_ports   = [5432],               # THROUGH these ports
     target_tags = ["app-servers"],      # FROM these VMs
-    priority    = 1000
   }]
 }
--- projects/my-service-project/ingress.tf
+++ projects/my-service-project/ingress.tf
@@ -1,14 +1,13 @@
 module "web_access" {
   source = "modules/ingress_allow"
 
-  project_id    = "team-project-123"
-  network_name  = "secure-vpc"
+  project_id    = "eminent-torch-451100-r2"
+  network_name  = "default"
 
   ingress_rules = [{
     rule_name   = "ingress-web-https",  # Direction in name
     from_ranges = ["203.0.113.0/24"],   # FROM these IPs
     to_ports    = [443],                # TO these ports
     target_tags = ["web-servers"],      # ON these VMs
-    priority    = 1000
   }]
 }
