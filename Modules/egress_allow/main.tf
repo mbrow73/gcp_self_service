50014971ac56e67cf0c077d1a65526786e73d887
mbrow73 <fatsaltytryhard@gmail.com>
2025-02-15 18:50:14 -0700

    adding stuff

--- Modules/egress_allow/main.tf
+++ Modules/egress_allow/main.tf
@@ -1,27 +1,25 @@
-variable "ingress_rules" {
+variable "egress_rules" {
   type = list(object({
-    rule_name      = string   # Format: "ingress-<purpose>"
-    from_ranges    = list(string)
-    to_ports       = list(number)
-    target_tags    = list(string)
-    priority       = number
+    rule_name   = string
+    to_ranges   = list(string)
+    via_ports   = list(number)
+    target_tags = list(string)
   }))
 }
 
-resource "google_compute_firewall" "ingress" {
-  for_each = { for rule in var.ingress_rules : rule.rule_name => rule }
+resource "google_compute_firewall" "egress" {
+  for_each = { for rule in var.egress_rules : rule.rule_name => rule }
 
   name        = each.value.rule_name
-  direction   = "INGRESS"  # Explicit direction in code
-  source_ranges = each.value.from_ranges
-  target_tags   = each.value.target_tags
-  
-  allow { 
+  direction   = "EGRESS"
+  destination_ranges = each.value.to_ranges
+  target_tags = each.value.target_tags
+
+  allow {
     protocol = "tcp"
-    ports    = each.value.to_ports 
+    ports    = each.value.via_ports
   }
-  
-  # Common parameters
+
   project = var.project_id
   network = var.network_name
   priority = 1000
