54ce61aabec741f15dfcfcb9a1b01a13d1b17a4a
mbrow73 <fatsaltytryhard@gmail.com>
2025-02-15 18:16:26 -0700

    adding stuff

--- main.tf
+++ main.tf
@@ -6,3 +6,8 @@ terraform {
     }
   }
 }
+provider "google" {
+      credentials = (var.GOOGLE_CREDENTIALS)
+      project     = (var.project)
+      region      = "us-central1"
+}
--- provider.tf
+++
@@ -1,5 +0,0 @@
-provider "google" {
-      credentials = (var.GOOGLE_CREDENTIALS)
-      project     = (var.project)
-      region      = "us-central1"
-}
