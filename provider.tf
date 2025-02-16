ea4645f1a288726e8314a42020626a6681842005
mbrow73 <fatsaltytryhard@gmail.com>
2025-02-15 18:13:14 -0700

    fixing stuff

--- provider.tf
+++ provider.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "6.20.0"
-    }
-  }
-}
-
 provider "google" {
       credentials = (var.GOOGLE_CREDENTIALS)
       project     = (var.project)
