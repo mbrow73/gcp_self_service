64e6c8e4731596f3fd7fbcd0edf9417ceb63c77c
mbrow73 <fatsaltytryhard@gmail.com>
2025-02-16 09:38:59 -0700

    idk

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "firewall_rules" {
   description = "Map of created firewall rules"
-  value       = { for k, v in module.team_firewall_rules : k => v }
+  value       = { for k, v in module.firewall_rule : k => v }
 }
