39afee15d49f93fcb34b5361f87334c54f8561e6
mbrow73 <fatsaltytryhard@gmail.com>
2025-02-23 11:52:01 -0700

    creating folder

--- variables.tf
+++ variables.tf
@@ -12,5 +12,5 @@ variable "GOOGLE_CREDENTIALS" {
 variable "folder_id" {
   description = "The folder ID under which the projects reside"
   type        = string
-  default     = "267070050069"  # Replace with your actual folder ID
+  default     = "folders/267070050069"  # Replace with your actual folder ID
 }
